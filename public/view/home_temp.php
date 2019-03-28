aba4643f8f5abd06fc0c922c7b764c0889218156
Bruno <bprates10@gmail.com>
2019-03-28 00:30:18 -0300

    Continuação commit - descrição projeto

--- public/view/home_temp.php
+++ public/view/home_temp.php
@@ -9,307 +9,46 @@
 ?>
 
 <!-- remover quando possivel -->
-<div class="container-fluid" id="div_principal">
-    <div class="row">1</div>
-    <div class="row">1</div>
-    <div class="row">1</div>
-    <div class="row">1</div>
-    <div class="row">1</div>
-    <div class="row">1</div>
-    <div class="row">1</div>
-    <div class="row">1</div>
-    <div class="row">1</div>
-    <div class="row">1</div>
-    <div class="row">1</div>
-    <div class="row">1</div>
-    <div class="row">1</div>
-    <div class="row">1</div>
-    <div class="row">1</div>
-    <div class="row">1</div>
-    <div class="row">1</div>
-    <div class="row">1</div>
-    <div class="row">1</div>
-    <div class="row">1</div>
-    <div class="row">1</div>
-    <div class="row">1</div>
-    <div class="row">1</div>
-    <div class="row">1</div>
-    <!-- Page Heading - ->
-    <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
-        <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
-    </div>
 
-    <! -- Content Row - ->
-    <div class="row">
+<div class="div_principal">
+    <p> A solução apresentada possui:</p>
+    <br/>
+    <p> - Dump do banco de dados PostgreSQL, versão 9.4, cujas credenciais são:</p>
+    <p> > Alias: pdv_softexpert</p>
+    <p> > Password: 10</p>
+    <p> - Criação de tabelas dinâmicas</p> e em tempo de execução</p>
+    <p> - Menu de Cadastro de produtos</p>
+    <p> - Menu de Cadastro de Categorias</p>
+    <p> - Menu de Cadastro de Impostos</p>
+    <p> - Tela de vendas, onde:
+    <p> > Selecionando o produto, é permitido informar a quantidade a ser vendida</p>
+    <p> > Informando a quantidade, o sistema calcula o total do produto, valor do imposto e total da venda</p>
+    <p> > Botão para adicionar mais produtos na mesma NF</p>
 
-        <!  -- Earnings (Monthly) Card Example - ->
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-primary shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Earnings (Monthly)</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-calendar fa-2x text-gray-300"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+    <p> Não foram utilizados frameworks, à exceção do bootstrap para o front-end.</p>
+    <p> Em back-end, foi utilizada a biblioteca JQuery do Javascript, embora o Ajax utilizado possa ser utilizado em JS sem a utilização da biblioteca.</p>
+    <p> O sistema foi criado utilizado o pattern MVC para distinguir as camadas da aplicação.</p>
+    <p> Foi utilizado padrão Singleton para acesso concorrente.</p>
+    <p> Os campos do front-end são validados em sua grande maioria (eu diria 100%, embora não pude realizar todos os testes pretendidos).</p>
+    <p> O módulo de Cadastros utiliza um único frame dinâmico que se adapta ao menu de cadastro pretendido (Produto, Categoria ou Imposto).</p>
+    <p> Um Produto sem Categoria não pode ser cadastrado;</p>
+    <p> Uma Categoria sem Imposto também não pode ser cadastrada;</p>
+    <p> Logo, o fluxo a ser seguido deve ser:</p>
+    <p> - Cadastro de imposto;</p>
+    <p> - Cadastro de categoria, vinculando o imposto;</p>
+    <p> - Cadastro de produto, vinculando a categoria.</p>
 
-        <! -- Earnings (Monthly) Card Example - ->
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-success shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Earnings (Annual)</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+    <p> A aplicação não está completa, pois abri mão dos seguintes componentes para cumprir o prazo estipulado:</p>
+    <p> - Não existem chaves estrangeiras nas tabelas de vendas, vendas_item e vendas_imposto.</p>
+    <p> - O botão Finalizar (a venda) não foi implementado.</p>
+    <p> - A tela inicial da aplicação não foi implementada.</p>
+    <p> - As grids dinâmicas, embora prontas, não foram implementadas. Entenda-se por grid dinâmica:</p>
+    <p> > Quando um item é cadastrado, o Ajax deveria atualizar assincronamente a grid de itens cadastrados.</p>
+    <p> > Quando uma categoria é cadastrada, o Ajax deveria atualizar assincronamente a grid de categorias cadastrados.</p>
+    <p> > Quando um imposto é cadastrado, o Ajax deveria atualizar assincronamente a grid de impostos cadastrados.</p>
+    <p> > Quando um item é adicionado à NF, o Ajax deveria atualizar assincronamente a grid de itens da NF cadastrados.</p>
 
-        <! -- Earnings (Monthly) Card Example - ->
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-info shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Tasks</div>
-                            <div class="row no-gutters align-items-center">
-                                <div class="col-auto">
-                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
-                                </div>
-                                <div class="col">
-                                    <div class="progress progress-sm mr-2">
-                                        <div class="progress-bar bg-info" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <!- - Pending Requests Card Example - ->
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-warning shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Pending Requests</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-comments fa-2x text-gray-300"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!- - Content Row - ->
-
-    <div class="row">
-
-        <! -- Area Chart - ->
-        <div class="col-xl-8 col-lg-7">
-            <div class="card shadow mb-4">
-                <! -- Card Header - Dropdown - ->
-                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                    <h6 class="m-0 font-weight-bold text-primary">Earnings Overview</h6>
-                    <div class="dropdown no-arrow">
-                        <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
-                            <div class="dropdown-header">Dropdown Header:</div>
-                            <a class="dropdown-item" href="#">Action</a>
-                            <a class="dropdown-item" href="#">Another action</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">Something else here</a>
-                        </div>
-                    </div>
-                </div>
-                <! -- Card Body - ->
-                <div class="card-body">
-                    <div class="chart-area">
-                        <canvas id="myAreaChart"></canvas>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <!- - Pie Chart - ->
-        <div class="col-xl-4 col-lg-5">
-            <div class="card shadow mb-4">
-                <! -- Card Header - Dropdown - ->
-                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                    <h6 class="m-0 font-weight-bold text-primary">Revenue Sources</h6>
-                    <div class="dropdown no-arrow">
-                        <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
-                            <div class="dropdown-header">Dropdown Header:</div>
-                            <a class="dropdown-item" href="#">Action</a>
-                            <a class="dropdown-item" href="#">Another action</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">Something else here</a>
-                        </div>
-                    </div>
-                </div>
-                <!- - Card Body - ->
-                <div class="card-body">
-                    <div class="chart-pie pt-4 pb-2">
-                        <canvas id="myPieChart"></canvas>
-                    </div>
-                    <div class="mt-4 text-center small">
-                        <span class="mr-2">
-                          <i class="fas fa-circle text-primary"></i> Direct
-                        </span>
-                        <span class="mr-2">
-                          <i class="fas fa-circle text-success"></i> Social
-                        </span>
-                        <span class="mr-2">
-                          <i class="fas fa-circle text-info"></i> Referral
-                        </span>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <! -- Content Row - ->
-    <div class="row">
-
-        <! -- Content Column - ->
-        <div class="col-lg-6 mb-4">
-
-            <! -- Project Card Example - ->
-            <div class="card shadow mb-4">
-                <div class="card-header py-3">
-                    <h6 class="m-0 font-weight-bold text-primary">Projects</h6>
-                </div>
-                <div class="card-body">
-                    <h4 class="small font-weight-bold">Server Migration <span class="float-right">20%</span></h4>
-                    <div class="progress mb-4">
-                        <div class="progress-bar bg-danger" role="progressbar" style="width: 20%" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
-                    <h4 class="small font-weight-bold">Sales Tracking <span class="float-right">40%</span></h4>
-                    <div class="progress mb-4">
-                        <div class="progress-bar bg-warning" role="progressbar" style="width: 40%" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
-                    <h4 class="small font-weight-bold">Customer Database <span class="float-right">60%</span></h4>
-                    <div class="progress mb-4">
-                        <div class="progress-bar" role="progressbar" style="width: 60%" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
-                    <h4 class="small font-weight-bold">Payout Details <span class="float-right">80%</span></h4>
-                    <div class="progress mb-4">
-                        <div class="progress-bar bg-info" role="progressbar" style="width: 80%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
-                    <h4 class="small font-weight-bold">Account Setup <span class="float-right">Complete!</span></h4>
-                    <div class="progress">
-                        <div class="progress-bar bg-success" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
-                </div>
-            </div>
-
-            <! -- Color System - ->
-            <div class="row">
-                <div class="col-lg-6 mb-4">
-                    <div class="card bg-primary text-white shadow">
-                        <div class="card-body">
-                            Primary
-                            <div class="text-white-50 small">#4e73df</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-6 mb-4">
-                    <div class="card bg-success text-white shadow">
-                        <div class="card-body">
-                            Success
-                            <div class="text-white-50 small">#1cc88a</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-6 mb-4">
-                    <div class="card bg-info text-white shadow">
-                        <div class="card-body">
-                            Info
-                            <div class="text-white-50 small">#36b9cc</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-6 mb-4">
-                    <div class="card bg-warning text-white shadow">
-                        <div class="card-body">
-                            Warning
-                            <div class="text-white-50 small">#f6c23e</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-6 mb-4">
-                    <div class="card bg-danger text-white shadow">
-                        <div class="card-body">
-                            Danger
-                            <div class="text-white-50 small">#e74a3b</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-6 mb-4">
-                    <div class="card bg-secondary text-white shadow">
-                        <div class="card-body">
-                            Secondary
-                            <div class="text-white-50 small">#858796</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-        </div>
-
-        <div class="col-lg-6 mb-4">
-
-            <!- - Illustrations - ->
-            <div class="card shadow mb-4">
-                <div class="card-header py-3">
-                    <h6 class="m-0 font-weight-bold text-primary">Illustrations</h6>
-                </div>
-                <div class="card-body">
-                    <div class="text-center">
-                        <img class="img-fluid px-3 px-sm-4 mt-3 mb-4" style="width: 25rem;" src="img/undraw_posting_photo.svg" alt="">
-                    </div>
-                    <p>Add some quality, svg illustrations to your project courtesy of <a target="_blank" rel="nofollow" href="https://undraw.co/">unDraw</a>, a constantly updated collection of beautiful svg images that you can use completely free and without attribution!</p>
-                    <a target="_blank" rel="nofollow" href="https://undraw.co/">Browse Illustrations on unDraw &rarr;</a>
-                </div>
-            </div>
-
-            <! -- Approach - ->
-            <div class="card shadow mb-4">
-                <div class="card-header py-3">
-                    <h6 class="m-0 font-weight-bold text-primary">Development Approach</h6>
-                </div>
-                <div class="card-body">
-                    <p>SB Admin 2 makes extensive use of Bootstrap 4 utility classes in order to reduce CSS bloat and poor page performance. Custom CSS classes are used to create custom components and custom utility classes.</p>
-                    <p class="mb-0">Before working with this theme, you should become familiar with the Bootstrap framework, especially the utility classes.</p>
-                </div>
-            </div>
-
-        </div>
-    </div>
-    <!- - /.container-fluid -->
+    <p> No mais, espero que aprecie o projeto enviado. Embora incompleta, a aplicação cumpre com as exigências do projeto.</p>
+    <p> Em no máximo 2 dias úteis a aplicação estará completa, podendo ser encontrada em github.com/bprates10/pdv_softexpert.</p>
+    <p> Obrigado, até mais.</p>
 </div>
